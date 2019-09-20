ca2a0622f712c58d43c8491e8775115d7ceb65df
torianne02 <victoria.fluharty@gmail.com>
2019-09-20 10:09:49 -0700

    adds methods to startup class

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,34 @@
 class Startup
+  attr_accessor :name, :domain
+  attr_reader :founder
 
+  @@all = []
+
+  def initialize(name, domain, founder)
+    @name = name
+    @domain = domain 
+    @founder = founder 
+    @@all << self
+  end 
+
+  def self.all 
+    @@all 
+  end 
+
+  def domain=(domain) 
+    @domain = domain
+  end 
+
+  def pivot(domain, name)
+    @domain = domain
+    @name = name
+  end  
+
+  def self.find_by_founder(founder) 
+    @@all.find(founder)
+  end 
+
+  def self.domains
+    @@all.collect { |s| s.domain }
+  end 
 end
