9da120e420fb9666e22111bc17223631bb315efa
torianne02 <victoria.fluharty@gmail.com>
2019-09-20 10:20:04 -0700

    adds methods to venture_capitalists

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,21 @@
 class VentureCapitalist
+  attr_reader :name, :total_worth
+  @@all = []
 
+  def initialize(name, total_worth)
+    @name = name 
+    @total_worth = total_worth
+
+    @@all << self
+  end 
+
+  def self.all
+    @@all
+  end 
+
+  def self.tres_commas_club
+    tres_commas_club = []
+    @@all.collect { |v| tres_commas_club << v if v.total_worth >= 1000000000 }
+    return tres_commas_club
+  end 
 end
