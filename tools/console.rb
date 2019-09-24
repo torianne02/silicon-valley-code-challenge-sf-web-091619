9cbd275a4a9a74f877ef783e1a151324051dead5
torianne02 <victoria.fluharty@gmail.com>
2019-09-24 08:47:23 -0700

    updates seed data

--- tools/console.rb
+++ tools/console.rb
@@ -13,10 +13,10 @@ flatiron = Startup.new("Flatiron", "flatironschool.com", "Avi")
 general_assembly = Startup.new("General Assembly", "generalassemb.ly", "Adam")
 mark_cuban = VentureCapitalist.new("Mark Cuban", 1300000000)
 mr_wonderful = VentureCapitalist.new("Mr. Wonderful", 13000000000000)
-flatiron.sign_contract("Mark Cuban", "Pre-Seed", 12000)
-flatiron.sign_contract("Mark Cuban", "Seed", 5000)
-flatiron.sign_contract("Mr. Wonderful", "Seed", 70000)
-mark_cuban.offer_contract("General Assembly", "Seed", 35000)
+flatiron.sign_contract(mark_cuban, "Pre-Seed", 12000)
+flatiron.sign_contract(mark_cuban, "Seed", 5000)
+flatiron.sign_contract(mr_wonderful, "Seed", 70000)
+mark_cuban.offer_contract(general_assembly, "Seed", 35000)
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
